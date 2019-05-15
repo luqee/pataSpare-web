270ce1ef7d61b8fb2f28e8d828f1ba1d970e413a
Luke Nzang'u <saintlukke@gmail.com>
2019-05-15 14:10:37 +0300

    start landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import exterior from '../images/exterior.png';
+import interior from '../images/interior.png';
+import performance from '../images/performance.png';
+import accessories from '../images/accessories.png';
 class LandingPage extends Component {
     render(){
         return (
             <div className='landing'>
-                <p>Auto spares clinic LandingPage page</p>
+                <div className='banner'>
+                    <p>The Textbook Platform for Auto Parts Business</p>
+                    <p>The best eCommerce solution for auto parts</p>
+                </div>
+                <div className='search-bar'>
+                    <form className="example" action="action_page.php">
+                        <input type="text" placeholder="Search.." name="search" />
+                        <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+                    </form>
+                </div>
+                <div className='categories'>
+                    <div className='category'>
+                        <img src={exterior} alt='exterior' />
+                        <p>EXTERIOR</p>
+                    </div>
+                    <div className='category'>
+                        <img src={interior} alt='exterior' />
+                        <p>INTERIOR</p>
+                    </div>
+                    <div className='category'>
+                        <img src={performance} alt='exterior' />
+                        <p>PERFORMANCE</p>
+                    </div>
+                    <div className='category'>
+                        <img src={accessories} alt='exterior' />
+                        <p>ACCESSORIES</p>
+                    </div>
+                </div>
+                <div className=''></div>
             </div>
         );
     }
